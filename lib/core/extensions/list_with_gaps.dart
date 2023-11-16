1ea98cde3f702ff806b17299ec7891224e8ebd9b
Krzysztof Potrząsaj <krzysztof.potrzasaj@gmail.com>
2023-11-16 09:47:25 +0100

    Fix compilation issue

--- lib/core/extensions/list_with_gaps.dart
+++ lib/core/extensions/list_with_gaps.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:offside/core/extensions/expand_with.dart';
-import 'package:offside/presentation/widgets/gap.dart';
 
 extension ListWithGaps on Iterable<Widget> {
   Iterable<Widget> withGaps({required double gapSize}) {
-    return expandWith(Gap(size: gapSize));
+    return expandWith(Gap(gapSize));
   }
 }
