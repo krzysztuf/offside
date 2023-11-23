bf2a567af173594b5d61581b2bd639ba69b667d1
Krzysztof Potrząsaj <krzysztof.potrzasaj@gmail.com>
2023-11-23 22:57:39 +0100

    Add first skeleton for match preview

--- lib/presentation/pages/home_page/matches_sub_page.dart
+++ lib/presentation/pages/home_page/matches_sub_page.dart
@@ -3,30 +3,79 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import 'package:offside/core/extensions/theme_context_extension.dart';
+import 'package:offside/presentation/widgets/square.dart';
 
 class MatchesSubPage extends ConsumerWidget {
   const MatchesSubPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Gap(16),
-          Text(
-            DateFormat('d MMMM yyyy', 'pl').format(DateTime(2023, 6, 6)),
-            style: context.textTheme.titleLarge,
-          ),
-          const Gap(8),
-          const Card(
-            child: SizedBox(
-              width: double.infinity,
-              height: 120,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(32),
+            Text(
+              DateFormat('d MMMM yyyy', 'pl').format(DateTime(2023, 6, 6)),
+              style: context.textTheme.titleLarge,
             ),
-          )
-        ],
+            const Gap(8),
+            Card(
+              child: SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text('Polska', style: context.textTheme.titleMedium),
+                              const Gap(8),
+                              const CircleAvatar(
+                                backgroundColor: Colors.red,
+                                child: Text('PL'),
+                              )
+                            ],
+                          ),
+                          Text('-', style: context.textTheme.headlineSmall),
+                          Column(
+                            children: [
+                              Text('Niemcy', style: context.textTheme.titleMedium),
+                              const Gap(8),
+                              Square(
+                                size: 40,
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.black87,
+                                  child: Text(
+                                    'DE',
+                                    style: context.textTheme.bodySmall,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      const Gap(24),
+                      const Divider(height: 1),
+                      const Gap(24),
+                      TextButton(
+                        child: const Text('WYTYPUJ WYNIK'),
+                        onPressed: () {},
+                      ),
+                      const Gap(8),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
